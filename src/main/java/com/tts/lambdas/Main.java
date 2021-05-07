cb0bb9142fc5ac6f9b69b65b0c5591907bf93350
Yusuf-Ali-1 <62290174+Yusuf-Ali-1@users.noreply.github.com>
2021-05-07 13:52:21 -0400

    first commit

--- src/main/java/com/tts/lambdas/Main.java
+++ src/main/java/com/tts/lambdas/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 
+        // this is an anonymous inner class
         Functional myFunctional = new Functional() {
             @Override
             public void doSomething() {
