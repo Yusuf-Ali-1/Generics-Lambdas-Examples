6275f1a439fd69adc603e01f5e4a11196676ee87
Yusuf-Ali-1 <62290174+Yusuf-Ali-1@users.noreply.github.com>
2021-05-07 13:51:54 -0400

    first commit

---
+++ src/main/java/com/tts/lambdas/Functional.java
@@ -0,0 +1,5 @@
+package com.tts.lambdas;
+
+public interface Functional {
+    void doSomething();
+}
--- src/main/java/com/tts/lambdas/Main.java
+++ src/main/java/com/tts/lambdas/Main.java
@@ -1,4 +1,22 @@
 package com.tts.lambdas;
 
 public class Main {
+
+    public static void main(String[] args) {
+
+        Functional myFunctional = new Functional() {
+            @Override
+            public void doSomething() {
+
+            }
+        };
+    }
+}
+
+class FunctionalImpl implements Functional{
+
+    @Override
+    public void doSomething() {
+
+    }
 }
