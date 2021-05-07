92e8b98b7322efb23549c14894644ec5601cec5e
Yusuf-Ali-1 <62290174+Yusuf-Ali-1@users.noreply.github.com>
2021-05-07 08:56:47 -0400

    first commit

--- src/main/java/com/tts/Main.java
+++ src/main/java/com/tts/Main.java
@@ -73,6 +73,7 @@ public class Main {
 
         System.out.println("\n=== Raw Generic Class ===");
         GenericClass genericClass = new GenericClass<>("this is the generic", "Bob");
+        System.out.println(genericClass);
 
 
 
