038b92acfbf1fb07f9d2018cc6d7ea44ab22268c
Yusuf-Ali-1 <62290174+Yusuf-Ali-1@users.noreply.github.com>
2021-05-07 14:10:04 -0400

    first commit

--- src/main/java/com/tts/lambdas/Functional.java
+++ src/main/java/com/tts/lambdas/Functional.java
@@ -1,5 +1,12 @@
 package com.tts.lambdas;
 
+
+// this us a functional interface
+// it can only have a single abstract method (SAM)
+@FunctionalInterface
 public interface Functional {
     void doSomething();
+
+    // I cannot have two abstract methods inside of this interface
+    // void doSomethingElse();
 }
---
+++ src/main/java/com/tts/lambdas/OtherFunctional.java
@@ -0,0 +1,7 @@
+package com.tts.lambdas;
+
+public interface OtherFunctional {
+
+    int operation(int x, int y);
+
+}
